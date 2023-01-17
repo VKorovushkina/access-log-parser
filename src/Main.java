2525fd5c0f46c33155497e6b6547461f7260db32
Veronika Korovushkina <VKorovushkina@inno.tech>
2023-01-18 01:36:02 +0300

    task 4.1 (module 6)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,44 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+        int min=999;
+        int max=0;
+        System.out.print("Введите путь к файлу: ");
+        String path = new Scanner(System.in).nextLine();
+        File file = new File(path);
+        boolean fileExists = file.exists();
+        boolean isDirectory = file.isDirectory();
+        if ((!fileExists) || (isDirectory)) {
+            System.out.println("Путь указан неверно, либо указан путь к папке");
+            return;
+        }
+        else{
+            System.out.println("Путь указан верно");
+        }
+        try {
+            FileReader fileReader = new FileReader(path);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.length()>1024){
+                    throw new RuntimeException("Длина строки больше 1024 символа");
+                }
+                int length = line.length();
+                if (length>max)
+                    max=length;
+                if (length<min)
+                    min=length;
+                count++;
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Количество строк в файле:"+count);
+        System.out.println("Длина самой длинной строки:"+max);
+        System.out.println("Длина самой короткой строки:"+min);
     }
 }
+
